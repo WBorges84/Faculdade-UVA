a23d6f76526d90f399ce04a8b410658683d44eb4
WBorges84 <willian.borges@hotmail.com.br>
2022-10-20 01:04:29 -0300

    bbb

---
+++ matriz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+main()
+{
+    int lin, col, tab;
+    int mat[4][4];
+    for (lin = 0; lin <= 3; lin++ )
+    {
+        for (col = 0; col <=3 ; col++)
+        {
+            printf("Digite ELEMENTO da linha %d, coluna %d da matriz: ", lin+1, col+1);
+            //aqui o scan preenche a matriz
+            scanf("%d", &mat[lin][col]);
+        
+        }
+
+        
+    }
+
+    //Imprimindo MAtriz
+
+    printf("Matriz \n");
+    for (lin= 0; lin <= 3; lin++)
+    {
+        for (col = 0; col <=3 ; col++)
+        {
+            printf("%d\t", mat[lin][col]);
+            printf("\n\n");
+            
+        }
+        
+    }
+    
+    
+
+}
