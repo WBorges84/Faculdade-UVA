c6332d3c2297786fdc4deba2a359d5b10f41dd05
Willian.Borges <109312625+WBorges84@users.noreply.github.com>
2022-10-21 10:48:47 -0300

    av1

--- av1.c
+++ av1.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
     float v1, v2,soma, subtracao, multiplicacao, divisao, result;
     int opc;
     char *txt;
-    //setlocale(LC_ALL, "Portugues");
+    setlocale(LC_ALL, "Portuguese");
     printf("\n\t Calculadora AV1");
     printf("\n Digite o primeiro valor a ser calculado\n");
     scanf("%f", &v1);
     printf("\n Digite o segundo valor a ser calculado\n");
     scanf("%f", &v2);
-    //system("cls");
     printf("\n O que deseja fazer com os valores digitados?\n\n");
     printf("Opcao 1 - SOMA \n");
-    printf("Opcao 2 - SUBTRACAO \n");
-    printf("Opcao 3 - MuLTIPLICACAO \n");
-    printf("Opcao 4 - DIVISAO \n\n");
+    printf("Opcao 2 - SUBTRA«√O \n");
+    printf("Opcao 3 - MuLTIPLICA«√O \n");
+    printf("Opcao 4 - DIVIS√O \n\n");
     scanf("%d", &opc);
 
     if (opc == 1)
-        result = v1 + v2;
-        txt = "SOMA";  
-    
+        {
+        result = v1 + v2;	
+    	txt = "SOMA";
+  		}
+    	
     if (opc == 2)
-        result = v1 - v2;
-        txt = "DIVIS√ÉO";
+    	{
+    	result = v1 - v2;	
+    	txt = "SUBTRA«√O";
+  		}
+  		
+  	if (opc == 3)
+    	{
+    	result = v1 * v2;	
+    	txt = "MULTIPLICA«√O";
+  		}
+	
+	if (opc == 4)
+    	{
+    	result = v1 / v2;	
+    	txt = "DIVIS√O";
+  		}	
+        
+        
+    printf("\n\t ******* Escolhido a %s", txt);
+    printf(" o resultado È : %.1f *******\n\n", result);
     
-
-    printf("Escolhido e %s o resultado √© :", txt);
-    //printf("O resultado e = %f", result);
-
-    //printf("Escolhido %s e o resultado √© : %.1f",txt, result);         
-   return 0;    
+	return 0;    
 }
