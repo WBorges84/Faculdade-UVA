6444a3358693427a2154a5b1de13c32364d3b3fb
WBorges84 <willian.borges@hotmail.com.br>
2022-10-23 21:26:22 -0300

    1

---
+++ teste.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	int num1, num2;
+	int operacao;
+	setlocale(LC_ALL, "Portuguese");
+	
+	printf("\n\t Calculadora AV1");	
+	
+	printf("\nSoma = 1 \n");
+	printf("\nSubtração = 2 \n");
+	printf("\nDivisão = 3 \n");
+	printf("\nMultiplicação = 4 \n");
+	scanf("%f", &operacao);
+	
+		printf("operacao");
+	
+}
