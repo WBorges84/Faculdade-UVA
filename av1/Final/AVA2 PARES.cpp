b9c29ac83efd72c75ba102b36fa5d8a553ac2ca9
WBorges84 <willian.borges@hotmail.com.br>
2022-10-29 14:49:45 -0300

    12

---
+++ av1/Final/AVA2 PARES.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//ALUNO : WILLIAN BORGES DE MELLO
+//Matricula : 20224301080 
+//TURMA : 2022.4
+
+int main()
+{
+	int numIni, numFim ;
+
+	setlocale(LC_ALL, "Portuguese");
+	printf("\t****AVA2 Números Pares e Ímpares ****\n\n");
+	//INTRODUÇÃO À LÓGICA DE PROGRAMAÇÃO j
+
+
+
+
+}
+
