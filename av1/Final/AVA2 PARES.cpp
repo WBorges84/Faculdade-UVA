056ffe59438e915580ce2dba4687dff3e409d511
WBorges84 <willian.borges@hotmail.com.br>
2022-10-30 00:04:09 -0300

    321

--- av1/Final/AVA2 PARES.cpp
+++ av1/Final/AVA2 PARES.cpp
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 #include <locale.h>
 
+
+int main()
+{
+//INTRODUÇÃO À LÓGICA DE PROGRAMAÇÃO 
+//AVA 2
 //ALUNO : WILLIAN BORGES DE MELLO
 //Matricula : 20224301080 
 //TURMA : 2022.4
 
-int main()
-{
 	int numIni, numFim ;
 
 	setlocale(LC_ALL, "Portuguese");
 	printf("\t****AVA2 Números Pares e Ímpares ****\n\n");
-	//INTRODUÇÃO À LÓGICA DE PROGRAMAÇÃO j
+	printf("Digite o número inicial: ");
+	scanf("%d", numIni);
+	printf("Digite o número Final: ");
+	scanf("%d", numFim);
+	
 
 
 
